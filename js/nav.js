f2c245ebc9717dc402128b96b7176ddad9ef6c9a
Fajar Agung <fajar.agngn@gmail.com>
2023-01-20 13:17:10 +0700

    ✧*｡٩(ˊᗜˋ*)و✧*｡ Fri Jan 20 13:17:10 WIB 2023

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,8 @@
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  if (x.className === "navbar") {
     x.className += " responsive";
   } else {
-    x.className = "topnav";
+    x.className = "navbar";
   }
 }
