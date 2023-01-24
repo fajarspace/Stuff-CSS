06cb2d3afad823a1cf64e5f032052b7da89b0cb9
Fajar Agung <fajar.agngn@gmail.com>
2023-01-24 14:08:12 +0700

    styling themes

--- dist/js/stuff.js
+++ dist/js/stuff.js
@@ -1,6 +1,6 @@
 // Imports
 import darkMode from './src/darkmode';
-import customDarkmode from "./src/custom-darkmode";
+// import customDarkmode from "./src/custom-darkmode";
 import toggleNavigation from './src/toggle-navigation';
 import copyButtonLabel from "./src/copy";
 
@@ -8,17 +8,12 @@ import copyButtonLabel from "./src/copy";
 darkMode.addButton({
   tag: 'li',
   class: 'contrast darkmode',
-  target: 'button',
-});
-customDarkmode.addButton({
-  tag: 'li',
-  class: 'contrast darkmode',
-  target: 'button',
+  target: '.darkmode',
 });
 
 //darkmode
 darkMode.init();
-customDarkmode.init();
+// customDarkmode.init();
 
 // Toggle navigation
 toggleNavigation.init();
