34a1f6cc1ebd25308d276ca27c72bf6be73a2df1
Fajar Agung <fajar.agngn@gmail.com>
2023-02-14 00:35:49 +0700

    update: typeit.js

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -7,16 +7,35 @@ export default function Hero() {
       <h1 className="judul">
         Stuff CSS
       </h1>
-      <h2 className="subjudul">
+      <h2 className="subjudul" >
+        <TypeIt
+          options={{
+            speed: 40,
+            loop: true,
+            loopDelay: 3000,
+            waitUntilVisible: true,
+          }}
+          getBeforeInit={(instance) => {
+            instance.type("Makes it easy to create responsive but minimalist Web pages.").pause(750).delete(10).pause(600).type("Class!");
+
+            // Remember to return it!
+            return instance;
+          }}
+        />
+      </h2>
+
+      {/* <h2 className="subjudul">
         <TypeIt
           options={{
             strings: ["Make it easy to create responsive but minimalist web pages. "],
             speed: 40,
+            loop: true,
+            loopDelay: 4000,
             waitUntilVisible: true,
           }}
         >
         </TypeIt>
-      </h2>
+      </h2> */}
     </>
 
   );
