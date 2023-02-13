b3d71690dbc5833a282982d6975db3cb105c1d81
Fajar Agung <fajar.agngn@gmail.com>
2023-02-14 00:08:57 +0700

    init

---
+++ src/pages/v/v1.1/dist/js/stuff.js
@@ -0,0 +1,17 @@
+// Imports
+import darkMode from './src/darkmode';
+// import customDarkmode from "./src/custom-darkmode";
+import toggleNavigation from './src/toggle-navigation';
+import copyButtonLabel from "./src/copy";
+// import flex from "./src/flex";
+
+
+
+// Toggle navigation
+toggleNavigation.init();
+
+
+
+
+// // Init
+// flex.init();
